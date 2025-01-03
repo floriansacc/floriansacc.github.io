f0da9d78368824ebf2fed001a1f14536467eb60d
floriansacc <florian.sacchetti@gmail.com>
2025-01-03 16:43:10 +0900

    🔧 initial config

--- v2/eslint.config.js
+++ v2/eslint.config.js
@@ -1,28 +1,39 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import react from "eslint-plugin-react";
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ["dist"] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommendedTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
+    ],
+    files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        project: ["./tsconfig.node.json", "./tsconfig.app.json"],
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
+    settings: { react: { version: "18.3" } },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      // "react-hooks": reactHooks,
+      // "react-refresh": reactRefresh,
+      react,
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
+      // ...reactHooks.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...react.configs["jsx-runtime"].rules,
+      "react-refresh/only-export-components": [
+        "warn",
         { allowConstantExport: true },
       ],
     },
-  },
-)
+  }
+);
---
+++ v2/tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    screens: {
+      sm: { max: "600px" },
+      md: { min: "600px", max: "1250px" },
+      lg: { min: "1250px" },
+    },
+    extend: {
+      colors: {
+        line: "#e6e7e8",
+        disabled: "#9e9e9E",
+        bgmain: "#fafafb",
+        bgcolor: "#fafafa",
+        menuactive: "#f1f1fa",
+        menuhover: "#f1f1fa",
+        inputfocus: "#5e5ec6",
+        buttonclick: "#4341bb",
+      },
+      boxShadow: {
+        dim: " 0 0 0 200vmax rgba(0, 0, 0, .3)",
+      },
+      transitionProperty: {
+        navigator: "transform, width",
+      },
+    },
+  },
+  plugins: [],
+};
