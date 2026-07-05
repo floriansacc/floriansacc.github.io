9e815272c1988468d6b6d9edebe9960d71516920
Florian Sacchetti <florian.sacchetti@gmail.com>
2026-07-05 16:07:23 +0900

    💄 add text justification

--- src/pages/sections/Section2Career.vue
+++ src/pages/sections/Section2Career.vue
@@ -31,7 +31,7 @@
         </p>
 
         <div
-          class="text-label-grey flex flex-col gap-2 text-sm whitespace-pre-wrap sm:text-base"
+          class="text-label-grey flex flex-col gap-2 text-justify text-sm whitespace-pre-wrap sm:text-base md:text-left"
         >
           <i18n-t
             scope="global"
